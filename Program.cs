24be3e77785484201ca448b477c541b5fcaaa330
b1sergiu <sebu400@gmail.com>
2020-04-07 17:08:33 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using DiscordRPC;
+
+namespace Deezcord
+{
+    public class Program
+    {
+        static DiscordRpcClient client = new DiscordRpcClient(APIKeys.DiscordApplicationId);
+        private static void Main()
+        {
+            Console.WriteLine("Deezcord Plus - https://github.com/b1sergiu/deezcord-plus/");
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            Console.WriteLine("Initializing Discord...");
+            client.Initialize();
+            client.SetPresence(new RichPresence()
+            {
+                Assets = new Assets()
+                {
+                    LargeImageKey = "logo"
+                }
+            });
+            await StartPresence();
+        }
+
+        public static async Task StartPresence()
+        {
+            while (true)
+            {
+                Track track = await DeezerAPI.LastTrack();
+                UpdatePresence(track);
+                Console.WriteLine($"Currently playing: {track.Title} by {track.Artist.Name} - {track.Album.Title}");
+                await Task.Delay(30000);
+            }
+        }
+
+        public static void UpdatePresence(Track song)
+        {
+            client.SetPresence(new RichPresence()
+            {
+                Details = song.Title,
+                State = $"by {song.Artist.Name}",
+                Assets = new Assets()
+                {
+                    LargeImageKey = "logo",
+                    LargeImageText = song.Album.Title
+                }
+            });
+            client.Invoke();
+        }
+
+        public async Task Login()
+        {
+            await DeezerAPI.Authenticate();
+            User user = await DeezerAPI.User();
+            Console.WriteLine($"Deezer user: {user.Name}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Deezcord Plus
+Show what you're listening to on Discord. This is a fork of [Deezcord by CodeMyst](https://github.com/CodeMyst/deezcord).
+*Note: Because of the Deezer API it can only display the last song in your history, not the currently playing one.*
+### [Downloads on the Release page](https://github.com/b1sergiu/deezcord-plus/releases)
+#### Changes from the original Deezcord:
+- Uses [DiscordRPC by Lachee](https://github.com/Lachee/discord-rpc-csharp)
+- Removed Avalonia
+- Runs automatically
+
+![](https://i.imgur.com/CaYw5yo.png)
