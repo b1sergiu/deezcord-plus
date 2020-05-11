2d3c4d10b29bfb4ac4aabd494a9b186fdbf04926
b1sergiu <admin@b1sergiu.online>
2020-05-11 12:07:25 +0300

    Fixed SSL error

--- DeezerAPI.cs
+++ DeezerAPI.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 
 using Newtonsoft.Json;
-using System.Net.Http;
 
 namespace Deezcord
 {
@@ -15,50 +14,50 @@ namespace Deezcord
     {
         private const string redirectUri = "http://127.0.0.1:5000/";
         private const string authorizationEndpoint = "https://connect.deezer.com/oauth/auth.php";
-        private const string tokenRequestUri = "https://connect.deezer.com/oauth/access_token.php";     
+        private const string tokenRequestUri = "https://connect.deezer.com/oauth/access_token.php";
         private const string lastTrackEndpoint = "https://api.deezer.com/user/me/history";
         private const string userEndpoint = "https://api.deezer.com/user/me";
         private const string tokenPath = "token.txt";
 
-        public static string token;
+        private static string token;
 
         public static async Task Authenticate()
         {
-            Console.WriteLine ($"Redirect URI: {redirectUri}");
+            Console.WriteLine($"Redirect URI: {redirectUri}");
 
             HttpListener http = new HttpListener();
             http.Prefixes.Add(redirectUri);
-            Console.WriteLine ("Listening...");
+            Console.WriteLine("Listening...");
             http.Start();
 
             string authorizationRequest = $"https://connect.deezer.com/oauth/auth.php?app_id={APIKeys.DeezerClientId}&redirect_uri={Uri.EscapeDataString(redirectUri)}&perms=listening_history";
 
-            if (RuntimeInformation.IsOSPlatform (OSPlatform.Windows))
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 ProcessStartInfo psi = new ProcessStartInfo
                 {
                     UseShellExecute = true,
                     FileName = authorizationRequest
                 };
-                Process.Start (psi);
+                Process.Start(psi);
             }
-            else if (RuntimeInformation.IsOSPlatform (OSPlatform.Linux))
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
-                Process.Start ("xdg-open", authorizationRequest);
+                Process.Start("xdg-open", authorizationRequest);
             }
-            else if (RuntimeInformation.IsOSPlatform (OSPlatform.OSX))
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
-                Process.Start ("open", authorizationRequest);
+                Process.Start("open", authorizationRequest);
             }
 
             HttpListenerContext context = await http.GetContextAsync();
 
             HttpListenerResponse response = context.Response;
             const string responseString = "<html><head><script type=\"text/javascript\">function closeMe() { window.close(); }</script></head><body>Please return to the app.</body></html>";
-            byte[] buffer = Encoding.UTF8.GetBytes (responseString);
+            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
             response.ContentLength64 = buffer.Length;
             Stream responseOutput = response.OutputStream;
-            await responseOutput.WriteAsync (buffer, 0, buffer.Length).ContinueWith ((tasl) =>
+            await responseOutput.WriteAsync(buffer, 0, buffer.Length).ContinueWith((tasl) =>
             {
                 responseOutput.Close();
                 http.Stop();
@@ -67,7 +66,7 @@ namespace Deezcord
 
             if (context.Request.QueryString.Get("error") != null)
             {
-                Console.WriteLine($"OAuth authorization error: {context.Request.QueryString.Get ("error")}");
+                Console.WriteLine($"OAuth authorization error: {context.Request.QueryString.Get("error")}");
                 return;
             }
 
@@ -89,10 +88,10 @@ namespace Deezcord
             tokenRequest.Method = "POST";
             tokenRequest.ContentType = "application/x-www-form-urlencoded";
             tokenRequest.Accept = "application/json";
-            byte[] byteVersion = Encoding.ASCII.GetBytes (tokenRequestBody);
+            byte[] byteVersion = Encoding.ASCII.GetBytes(tokenRequestBody);
             tokenRequest.ContentLength = byteVersion.Length;
-            Stream stream = tokenRequest.GetRequestStream ();
-            await stream.WriteAsync (byteVersion, 0, byteVersion.Length);
+            Stream stream = tokenRequest.GetRequestStream();
+            await stream.WriteAsync(byteVersion, 0, byteVersion.Length);
             stream.Close();
 
             try
@@ -110,11 +109,11 @@ namespace Deezcord
             catch (WebException e)
             {
                 if (e.Status == WebExceptionStatus.ProtocolError)
-                {}
+                { }
 
                 if (e.Response is HttpWebResponse r)
                 {
-                    Console.WriteLine ($"HTTP: {response.StatusCode}");
+                    Console.WriteLine($"HTTP: {response.StatusCode}");
                     using (StreamReader reader = new StreamReader(r.GetResponseStream() ?? throw new Exception()))
                     {
                         string responseText = await reader.ReadToEndAsync();
@@ -127,7 +126,6 @@ namespace Deezcord
         public static async Task<Track> LastTrack()
         {
             string requestUri = $"{lastTrackEndpoint}?access_token={token}";
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
 
             HttpWebRequest userinfoRequest = (HttpWebRequest)WebRequest.Create(requestUri);
             userinfoRequest.Method = "GET";
@@ -135,19 +133,19 @@ namespace Deezcord
             userinfoRequest.Accept = "Accept=text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
 
             WebResponse userinfoResponse = await userinfoRequest.GetResponseAsync();
-            using (StreamReader userinfoResponseReader = new StreamReader(userinfoResponse.GetResponseStream() ?? throw new Exception ()))
+            using (StreamReader userinfoResponseReader = new StreamReader(userinfoResponse.GetResponseStream() ?? throw new Exception()))
             {
                 string userinfoResponseText = await userinfoResponseReader.ReadToEndAsync();
 
-                if (userinfoResponseText.Contains ("Invalid OAuth access token."))
+                if (userinfoResponseText.Contains("Invalid OAuth access token."))
                 {
-                    Console.WriteLine("Invalid OAuth access token.");
                     await Authenticate();
                     return await LastTrack();
                 }
 
-                History tracks = JsonConvert.DeserializeObject<History> (userinfoResponseText);
-                return tracks.Tracks [0];
+                History tracks = JsonConvert.DeserializeObject<History>(userinfoResponseText);
+
+                return tracks.Tracks[0];
             }
         }
 
@@ -162,18 +160,17 @@ namespace Deezcord
 
             WebResponse userinfoResponse = await userinfoRequest.GetResponseAsync();
 
-            using (StreamReader userinfoResponseReader = new StreamReader(userinfoResponse.GetResponseStream() ?? throw new Exception ()))
+            using (StreamReader userinfoResponseReader = new StreamReader(userinfoResponse.GetResponseStream() ?? throw new Exception()))
             {
                 string userinfoResponseText = await userinfoResponseReader.ReadToEndAsync();
 
-                if (userinfoResponseText.Contains ("Invalid OAuth access token."))
+                if (userinfoResponseText.Contains("Invalid OAuth access token."))
                 {
-                    Console.WriteLine("Invalid OAuth access token.");
                     await Authenticate();
                     return await User();
                 }
 
-                User user = JsonConvert.DeserializeObject<User> (userinfoResponseText);
+                User user = JsonConvert.DeserializeObject<User>(userinfoResponseText);
 
                 return user;
             }
